29e1f8f390668946fafdc63b529bbf147c6e20a2
Feliks Nazaryan <Feliks123@yandex.ru>
2026-07-23 20:36:01 +0400

    Project ready

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@
  * @returns {number}
  */
 function calculateSimpleRevenue(purchase, _product) {
+    const {discount, quantity, sale_price} = purchase;
+    return sale_price * quantity * (1 - discount / 100)
    // @TODO: Расчет выручки от операции
 }
 
@@ -16,6 +18,16 @@ function calculateSimpleRevenue(purchase, _product) {
  * @returns {number}
  */
 function calculateBonusByProfit(index, total, seller) {
+    const {profit} = seller;
+    if (index === 0) {
+        return profit * 0.15
+    } else if (index === 1 || index === 2) {
+        return profit * 0.1
+    }else if (index === total -1) {
+        return 0;
+    } else {
+        return profit * 0.05
+    }
     // @TODO: Расчет бонуса от позиции в рейтинге
 }
 
@@ -26,10 +38,114 @@ function calculateBonusByProfit(index, total, seller) {
  * @returns {{revenue, top_products, bonus, name, sales_count, profit, seller_id}[]}
  */
 function analyzeSalesData(data, options) {
-    // @TODO: Проверка входных данных
+    const {calculateRevenue, calculateBonus} = options;
 
-    // @TODO: Проверка наличия опций
+    if (!data || 
+        !Array.isArray(data.sellers) ||
+        !Array.isArray(data.products) ||
+        !Array.isArray(data.purchase_records)
+) {
+    throw new Error("Некорректные входные данные")
+}
 
+  if (
+    data.sellers.length === 0 ||
+    data.products.length === 0 ||
+    data.purchase_records.length === 0 
+ ) { 
+    throw new Error("Переданные коллекции пустые")
+ }
+
+ if (
+    !options || 
+    typeof calculateRevenue !== "function" ||
+    typeof calculateBonus !== "function" 
+ ) {
+    throw new Error("В объекте options должны быть переданы функции calculateRevenue и calculateBonus.")
+ }
+   
+    const sellerStats = data.sellers.map(seller => ({
+   seller_id: seller.id,
+   name: `${seller.first_name} ${seller.last_name}`,
+   revenue: 0,
+   profit: 0,
+   sales_count: 0,
+   products_sold: {}, 
+}));
+
+    
+
+    const sellerIndex = Object.fromEntries(
+    sellerStats.map((seller) => [seller.seller_id, seller])
+  );
+
+    const productIndex = Object.fromEntries(
+    data.products.map((product) => [product.sku, product])
+  );
+
+
+
+  data.purchase_records.forEach((record) => {
+    const seller = sellerIndex[record.seller_id]; 
+    if (!seller) return; 
+
+    
+    seller.sales_count += 1;
+
+    
+    seller.revenue += record.total_amount;
+
+    
+    record.items.forEach((item) => {
+      const product = productIndex[item.sku]; 
+      if (!product) return; 
+
+    
+      const revenue = calculateRevenue(item, product);
+
+      
+      const profit = revenue - product.purchase_price * item.quantity;
+
+    
+      seller.profit += profit;
+
+      
+      if (!seller.products_sold[item.sku]) {
+        seller.products_sold[item.sku] = 0;
+      }
+      seller.products_sold[item.sku] += item.quantity;
+    });
+  });
+
+  
+  sellerStats.sort((a, b) => b.profit - a.profit);
+
+  
+  sellerStats.forEach((seller, index) => {
+    
+    seller.bonus = options.calculateBonus(index, sellerStats.length, seller);
+
+    
+    seller.top_products = Object.entries(seller.products_sold)
+      .map(([sku, quantity]) => ({
+        sku,
+        quantity,
+      }))
+      .sort((a, b) => b.quantity - a.quantity)
+      .slice(0, 10);
+  });
+
+  
+  return sellerStats.map((seller) => ({
+    seller_id: seller.seller_id,
+    name: seller.name,  
+    revenue: +seller.revenue.toFixed(2),
+    profit: +seller.profit.toFixed(2),
+    sales_count: seller.sales_count,
+    top_products: seller.top_products,
+    bonus: +seller.bonus.toFixed(2),
+  }));
+}
     // @TODO: Подготовка промежуточных данных для сбора статистики
 
     // @TODO: Индексация продавцов и товаров для быстрого доступа
@@ -41,4 +157,4 @@ function analyzeSalesData(data, options) {
     // @TODO: Назначение премий на основе ранжирования
 
     // @TODO: Подготовка итоговой коллекции с нужными полями
-}
+
